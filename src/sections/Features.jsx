d06644d632e67897efe67f361db2b5fa734c9d83
Saravana priyan C <c.saravanapriyan@gmail.com>
2025-01-29 20:46:59 +0530

    Second Commit

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,5 +1,6 @@
 import { Element } from "react-scroll";
 import { features } from "../constants/index.js";
+import Button from "../components/Button";
 
 const Features = () => {
   return (
@@ -13,7 +14,8 @@ const Features = () => {
                 className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:gap-7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl"
               >
                 <div className="w-full flex justify-start items-start">
-                  <div className="">
+                  <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
+                    <div className="w-0.5 h-16 bg-s3" />
                     <img
                       src={icon}
                       className="size-28 object-contain"
@@ -22,6 +24,13 @@ const Features = () => {
                     {title}
                   </div>
                 </div>
+
+                <p className="caption mb-5 max-md:mb-6 max-md:h5">{caption}</p>
+                <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
+                  {title}
+                </h2>
+                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
+                <Button icon={button.Icon}>{button.title}</Button>
               </div>
             ))}
           </div>
